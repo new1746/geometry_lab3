aabe2bc01ca08df7102d26b5de485bd58bcd35fc
Vladislav Filimonov <new1746@mail.ru>
2019-05-20 16:43:11 +0700

    Changed

--- src/cross.c
+++ src/cross.c
@@ -5,9 +5,9 @@
 
 int Cross(int x1, int y1, int r1, int x2, int y2, int r2)
 {
-    int dis = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-    if (dis <= (r1 + r2))
-        return 1;
-    else
-        return 0;
+	int dis = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+	if (dis <= (r1 + r2))
+		return 1;
+	else
+		return 0;
 }
